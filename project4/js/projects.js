378f5e4aa0c10aa9d54bd4d8cc9f4517cf077f81
codechelle <49601776+codechelle@users.noreply.github.com>
2022-05-10 23:16:49 -0400

    upload project 4

---
+++ project4/js/projects.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#magazine').click(function() {
+        $('#magazine-info').fadeIn();
+    });
+    $('#coloring').click(function() {
+        $('#coloring-info').fadeIn();
+    });
+    $('#poster').click(function() {
+        $('#poster-info').fadeIn();
+    });
+    $('#movie').click(function() {
+        $('#movie-info').fadeIn();
+    });
+});
